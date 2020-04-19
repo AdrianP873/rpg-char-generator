6d1cf6cfa218600d25065f4fcb9c8b12ae70b2fc
AdrianP873 <31646350+AdrianP873@users.noreply.github.com>
2020-04-19 22:36:19 +1000

    delete character_form.py
    Replaced by forms.py. Left over after branch merge

--- character_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField
-
-class CreateCharacterForm(FlaskForm):
-    characterName = StringField('Character Name')
-    characterClass = RadioField('Label', choices=[('Knight', 'Knight'), ('Sorcerer','Sorcerer')])
-    createCharacter = SubmitField('Create character')
