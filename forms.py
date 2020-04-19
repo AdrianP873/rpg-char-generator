934378e1825ffb172923fef17096c1c300fe6a8f
AdrianP873 <adrian.m.plummer@gmail.com>
2020-04-19 13:32:03 +1000

    Added basic UI changes

--- forms.py
+++ forms.py
@@ -4,6 +4,15 @@ from wtforms import StringField, PasswordField, SubmitField, RadioField
 class SignUpForm(FlaskForm):
     username = StringField('Username')
     password = PasswordField('Password')
+    email = StringField('Email')
     submit = SubmitField('Sign up')
 
+class CreateCharacterForm(FlaskForm):
+    characterName = StringField('Character Name')
+    characterClass = RadioField('Label', choices=[('Knight', 'Knight'), ('Sorcerer','Sorcerer')])
+    createCharacter = SubmitField('Create character')
 
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
--- generate_chars.py
+++ generate_chars.py
@@ -1,7 +1,6 @@
 import flask
 from flask import render_template, request, jsonify
-from forms import SignUpForm
-from character_form import CreateCharacterForm
+from forms import SignUpForm, CreateCharacterForm, LoginForm
 import sqlite3
 import requests
 
@@ -15,8 +14,27 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-# Create character page
-@app.route('/', methods=['GET', 'POST'])
+# Prevent caching during development
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-store"
+    response.headers["Expires"] = '0'
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Landing page - login
+@app.route('/', methods=['GET'])
+def login():
+    form = LoginForm()
+    if form.is_submitted():
+        data = form.request
+        return render_template('login.html', data=data)
+    return render_template('login.html', form=form)
+
+
+# Create a character and store it in the database
+@app.route('/create', methods=['GET', 'POST'])
 def home():
     
     # Instantiate form
@@ -47,31 +65,36 @@ def home():
         cur.execute('INSERT INTO Characters VALUES (?,?,?,?,?,?,?)', resultList)
         conn.commit()
         conn.close()
-        
 
         return render_template('character.html', result=result, vocation=vocation)
     return render_template('create_character.html', name='Adrian', form=form)
 
 
-
-# Get characters from database
-@app.route('/search', methods=['POST'])
+# Search for a character in the database
+@app.route('/search', methods=['GET','POST'])
 def searchCharacter():
-    data = request.get_json()
-    character = data.get('query')
-    
-    resList = [character]
 
-    conn = sqlite3.connect('rpg.db')
-    conn.row_factory = dict_factory
-    cur =  conn.cursor()
+    if request.method == 'POST':
+        data = request.get_json()
+        character = data.get('query')
     
-    char = cur.execute('SELECT * FROM Characters WHERE Name = ? ;', resList).fetchone()
+        resList = [character]
 
-    if char != None:
-        return render_template('character.html', char=char)
-    else:
-        return "That character does not exist"
+        conn = sqlite3.connect('rpg.db')
+        conn.row_factory = dict_factory
+        cur =  conn.cursor()
+    
+        char = cur.execute('SELECT * FROM Characters WHERE Name = ? ;', resList).fetchone()
+
+        if char != None:
+            return render_template('character.html', char=char)
+        else:
+            return "That character does not exist"
+    return render_template('search.html')
+
+
+
+
 
 # =======================================================
 
@@ -83,4 +106,4 @@ def signup():
         return render_template('user.html', result=result)
     return render_template('signup.html', form=form)
 
-app.run(debug=True)
+app.run(debug=False)
