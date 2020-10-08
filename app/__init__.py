fb5b2f42e6f868d3f276ad71321e9b5ecd3cbeb7
AdrianP873 <adrian.m.plummer@gmail.com>
2020-10-09 02:35:34 +1100

    Implemented AWS X-ray

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config["DEBUG"] = True
+
+xray_recorder.configure(service='app')
+XRayMiddleware(app, xray_recorder)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
