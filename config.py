dc4fde94050f112c6fd4074e9d43b8738fe6d376
AdrianP873 <adrian.m.plummer@gmail.com>
2020-10-09 02:40:46 +1100

    added fallback secret key

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Define configuration settings as class variables inside a config class
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my secret key'
 
     # Store database URL in environment variable or fall back to using sqlite with the database in basedir
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
